ef374abb8bcbd38f0be1b7852f5d0acf6c7a18c9
Olzaq <olzaq@localhost>
2020-04-25 23:38:55 +0300

    Add function descriptions to own-functions.el
    Silence Flycheck

--- own-functions.el
+++ own-functions.el
@@ -4,12 +4,15 @@
 
 ;;; Code:
 
-(defun to-underscore () (interactive)
+(defun to-underscore ()
+  "Separate word at point with '_' at each capital letter."
+  (interactive)
     (progn (replace-regexp "\\([A-Z]\\)" "_\\1" nil
     (region-beginning) (region-end)) (downcase-region (region-beginning) (region-end)))
 )
 
 (defun split-name (s)
+  "Split given string S at each lower-to-upper-case boundary."
   (split-string
    (let ((case-fold-search nil))
 	 (downcase
@@ -17,17 +20,24 @@
    "[^A-Za-z0-9]+"))
 
 (defun capitalize-all-but-first (l)
+  "Capitalize all words in L expect the first one."
   (let ((first (car l))
         (rest (cdr l)))
     (cons first (mapcar 'capitalize rest)) ))
 
-(defun camelCase  (s) (mapconcat 'identity (capitalize-all-but-first (split-name s)) ""))
-(defun CamelCase  (s) (mapconcat 'capitalize (split-name s) ""))
-(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
-(defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
-(defun colonize   (s) (mapconcat 'capitalize (split-name s) "::"))
+(defun camelCase  (s) "Convert S to camelCaseStyle."
+  (mapconcat 'identity (capitalize-all-but-first (split-name s)) ""))
+(defun CamelCase  (s) "Convert S to CamelCaseStyle."
+  (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) "Convert S to under_score_style."
+  (mapconcat 'downcase   (split-name s) "_"))
+(defun dasherize  (s) "Convert S to dash-style."
+  (mapconcat 'downcase   (split-name s) "-"))
+(defun colonize   (s) "Convert S to Scope::Resolution::Style."
+  (mapconcat 'capitalize (split-name s) "::"))
 
 (defun camelscore-word-at-point ()
+  "Cycle naming styles."
   (interactive)
   (let* ((case-fold-search nil)
 	     (beg (and (skip-chars-backward "[:alnum:]:_-") (point)))
@@ -37,6 +47,7 @@
 	(if cml (progn (delete-region beg end) (insert cml))) ))
 
 (defun camelscore (s)
+  "Select next matching style for given string S."
   (cond ((string-match-p "\\(?:[a-z]+_\\)+[a-z]+" s)	        (dasherize  s))
         ((string-match-p "\\(?:[a-z]+-\\)+[a-z]+" s)	        (camelCase  s))
 	    ((string-match-p "\\b[a-z]+\\(?:[A-Z][a-z]+\\)+" s)	    (CamelCase  s))
