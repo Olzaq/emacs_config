aefb130cee429e81329f6b14d93189cb62cfb988
Olzaq <olzaqbit@gmail.com>
2020-05-06 23:48:28 +0300

    Add functions from emacs.stackexchange
    Source:
    https://emacs.stackexchange.com/questions/751/fundamentals-of-multiple-cursors

--- own-functions.el
+++ own-functions.el
@@ -54,6 +54,34 @@
 	    ((string-match-p "\\(?:[A-Z][a-z]+::\\)+[A-Z][a-z]+" s)	(underscore s))
         ((string-match-p "\\(?:[A-Z][a-z]+\\)+$"  s)	        (colonize   s)) ))
 
+(defun prompt-for-insert ()
+  "Prompt for inserting text."
+  (interactive)
+  (insert (read-string "Insert: ")))
+
+(defun eval-and-replace ()
+  "Evaluate exp and replace it with result."
+  (interactive)
+  (let ((value (eval (elisp--preceding-sexp))))
+    (kill-sexp -1)
+    (insert (format "%s" value))))
+
+(defun my-increment-number-decimal (&optional arg)
+  "Increment the number forward from point by 'ARG'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
 
 (provide 'own-functions)
 ;;; own-functions.el ends here
