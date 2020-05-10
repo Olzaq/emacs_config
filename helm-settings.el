a62b33f8bb8c64b299a21d0867700968864d4bbe
Olzaq <olzaq@localhost>
2020-05-11 00:07:48 +0300

    Move helm setup to own file

---
+++ helm-settings.el
@@ -0,0 +1,39 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;; Helm setup
+
+;; this variables must be set before load helm-gtags
+(setq helm-gtags-prefix-key "\C-t")
+
+(require 'helm-config)
+(require 'helm-gtags)
+(require 'helm-grep)
+
+(helm-mode 1)
+;;; Enable helm-gtags-mode
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+;; customize
+(custom-set-variables
+ '(helm-gtags-path-style 'relative)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-auto-update t))
+
+;; key bindings
+(with-eval-after-load 'helm-gtags
+  (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+  (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+  (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+  (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
+
+(provide 'helm-settings)
+;;; helm-settings.el ends here
