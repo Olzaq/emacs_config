08d021656957779dc7be8166f2b77d7e56244944
Olzaq <olzaqbit@gmail.com>
2022-11-14 12:57:29 +0200

    Disable ccls stuff

--- c-and-cpp-mode.el
+++ c-and-cpp-mode.el
@@ -48,11 +48,11 @@
                 lsp-ui-sideline-update-mode 'line
                 lsp-ui-doc-show-with-cursor t))
 
-  (use-package ccls
-    :after lsp-mode
-    :config (setq ccls-executable "~/src/CCLS_install/bin/ccls")
-    :hook ((c-mode c++-mode objc-mode cuda-mode) .
-           (lambda () (require 'ccls) (lsp))))
+  ;; (use-package ccls
+  ;;   :after lsp-mode
+  ;;   :config (setq ccls-executable "~/src/CCLS_install/bin/ccls")
+  ;;   :hook ((c-mode c++-mode objc-mode cuda-mode) .
+  ;;          (lambda () (require 'ccls) (lsp))))
 
   (add-hook 'c-mode-hook 'lsp)
   (add-hook 'c++-mode-hook 'lsp))
@@ -64,33 +64,33 @@
 ;;
 ;; Recommended helpers from lsp-mode instructions
 ;;
-(defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
-(defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
-(defun ccls/vars (kind) (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
-(defun ccls/base (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
-(defun ccls/derived (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
-(defun ccls/member (kind) (interactive) (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
+;; (defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+;; (defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
+;; (defun ccls/vars (kind) (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
+;; (defun ccls/base (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
+;; (defun ccls/derived (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
+;; (defun ccls/member (kind) (interactive) (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
 
-;; References w/ Role::Role
-(defun ccls/references-read () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-    (plist-put (lsp--text-document-position-params) :role 8)))
+;; ;; References w/ Role::Role
+;; (defun ccls/references-read () (interactive)
+;;   (lsp-ui-peek-find-custom "textDocument/references"
+;;     (plist-put (lsp--text-document-position-params) :role 8)))
 
-;; References w/ Role::Write
-(defun ccls/references-write ()
-  (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :role 16)))
+;; ;; References w/ Role::Write
+;; (defun ccls/references-write ()
+;;   (interactive)
+;;   (lsp-ui-peek-find-custom "textDocument/references"
+;;    (plist-put (lsp--text-document-position-params) :role 16)))
 
-;; References w/ Role::Dynamic bit (macro expansions)
-(defun ccls/references-macro () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :role 64)))
+;; ;; References w/ Role::Dynamic bit (macro expansions)
+;; (defun ccls/references-macro () (interactive)
+;;   (lsp-ui-peek-find-custom "textDocument/references"
+;;    (plist-put (lsp--text-document-position-params) :role 64)))
 
-;; References w/o Role::Call bit (e.g. where functions are taken addresses)
-(defun ccls/references-not-call () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :excludeRole 32)))
+;; ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+;; (defun ccls/references-not-call () (interactive)
+;;   (lsp-ui-peek-find-custom "textDocument/references"
+;;    (plist-put (lsp--text-document-position-params) :excludeRole 32)))
 
 ;; ccls/vars ccls/base ccls/derived ccls/members have a parameter while others are interactive.
 ;; (ccls/base 1) direct bases
